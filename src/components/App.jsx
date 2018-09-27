02e3aad8217a918e854a8bc523e4abbffe96adb6
Oksana Tkachenko <oksana@crystax.net>
2018-09-27 18:40:30 +0300

    added content to component MessageList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
 import MeassgeList from './MessageList.jsx';
 
+const DATA = [
+  {
+    senderId: 'jane',
+    text: 'Hey',
+  },
+  {
+    senderId: 'peter',
+    text: 'Good to hear you!',
+  },
+  {
+    senderId: 'jane',
+    text: 'Great! How about you?',
+  },
+]
+
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        Message
+      <div className='message-list'>
+        {DATA.map((message, index) =>{
+          return (
+            <div>
+              <div>
+               {message.senderId}
+             </div>
+             <div>
+               {message.text}
+             </div>
+            </div>
+          )
+        })}
       </div>
     )
   }
