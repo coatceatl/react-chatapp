8b0c5c6b1bba2a10b74004944f58d465de75b1a3
Oksana Tkachenko <oksana@crystax.net>
2018-09-27 17:53:56 +0300

    added component MessageList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
+import MeassgeList from './MessageList.jsx';
 
-const App = () => {
-  return <h1>This is a component React</h1>
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        Message
+      </div>
+    )
+  }
 }
 
 export default App
