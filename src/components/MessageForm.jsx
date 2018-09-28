5cd63d2021449601e6b44f291d19d45f6f135370
Oksana Tkachenko <oksana@crystax.net>
2018-09-28 09:46:12 +0300

    added constructor and bind for handler for setState

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -2,11 +2,22 @@ import React, {Component} from 'react';
 
 class MessageForm extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
   handleChange(e) {
-    console.log(e.target.value)
+    this.setState({
+      message: e.target.value
+    })
   }
 
   render() {
+    console.log(this.state.message)
     return (
       <form className='message-form'>
         <input
