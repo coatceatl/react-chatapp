64662d2aa768d186d015b628b0d05ff809fedf6b
Oksana Tkachenko <oksana@crystax.net>
2018-09-29 11:59:04 +0300

    added style for active room

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -81,6 +81,7 @@ class App extends Component {
     return (
       <div className='app'>
         <RoomList
+          roomId={this.state.roomId}
           subscribeToRoom={this.subscribeToRoom}
           rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]} />
         <MessageList messages={this.state.messages}/>
